7980512ca1b4a754024cff2b84fba8310aebce05
bendeyhe <108757682+bendeyhe@users.noreply.github.com>
2023-10-29 04:11:10 -0300

    Algunos ejemplos de la clase

--- app/src/main/java/ar/edu/itba/tpHciMobile/MainActivity.kt
+++ app/src/main/java/ar/edu/itba/tpHciMobile/MainActivity.kt
@@ -3,13 +3,17 @@ package ar.edu.itba.tpHciMobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ar.edu.itba.tpHciMobile.ui.theme.TP_HCI_MOBILETheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +22,57 @@ class MainActivity : ComponentActivity() {
         setContent {
             TP_HCI_MOBILETheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Greeting("Android")
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Column(modifier = Modifier.padding(24.dp)){
+            Text(
+                text = "Hello",
+            )
+            Text(
+                text = "$name!",
+                modifier = Modifier.padding(24.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TP_HCI_MOBILETheme {
-        Greeting("Android")
+        MyApp()
     }
+}
+
+@Composable
+fun MyText() {
+    val names = listOf("John", "Jane", "Tom")
+    Column(Modifier.padding(12.dp)) {
+        for(name in names) {
+            Text(text = name)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyTextPreview() {
+    MyText()
 }
