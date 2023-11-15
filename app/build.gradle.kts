066d2893fb9aa97fbc12e0e7cc2256bbb8724690
bendeyhe <108757682+bendeyhe@users.noreply.github.com>
2023-11-15 16:20:03 -0300

    Update sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ar.edu.itba.tpHciMobile"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ar.edu.itba.tpHciMobile"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
