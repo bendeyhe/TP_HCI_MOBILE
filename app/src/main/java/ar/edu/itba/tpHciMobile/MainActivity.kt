5337ae59678f768164901077706e89bcf8e5cc30
MatiasMutz <108437589+MatiasMutz@users.noreply.github.com>
2023-11-17 01:00:11 -0300

    deleted repeated import

--- app/src/main/java/ar/edu/itba/tpHciMobile/MainActivity.kt
+++ app/src/main/java/ar/edu/itba/tpHciMobile/MainActivity.kt
@@ -31,7 +31,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
