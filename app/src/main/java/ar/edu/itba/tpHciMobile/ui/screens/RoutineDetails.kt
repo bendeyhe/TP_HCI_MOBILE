c9639c2a98137eec9c4901855dcffd74e6304ce7
bendeyhe <108757682+bendeyhe@users.noreply.github.com>
2023-11-20 01:03:14 -0300

    ExpandableMenu for Routine Details

--- app/src/main/java/ar/edu/itba/tpHciMobile/ui/screens/RoutineDetails.kt
+++ app/src/main/java/ar/edu/itba/tpHciMobile/ui/screens/RoutineDetails.kt
@@ -1,18 +1,25 @@
 package ar.edu.itba.tpHciMobile.ui.screens
 
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -20,11 +27,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ar.edu.itba.tpHciMobile.R
@@ -42,6 +53,8 @@ fun RoutineDetails() {
 
 @Composable
 fun RoutineDetailsContent(modifier: Modifier) {
+
+    /*
     val cycles = listOf(
         "Entrada en Calor",
         "Principal",
@@ -90,6 +103,27 @@ fun RoutineDetailsContent(modifier: Modifier) {
             }
         }
     }
+
+
+     */
+    CollapsableLazyColumn(
+        sections = listOf(
+            CollapsableSection(
+                title = "Fruits A",
+                rows = listOf("Apple", "Apricots", "Avocado")
+            ),
+            CollapsableSection(
+                title = "Fruits B",
+                rows = listOf("Banana", "Blackberries", "Blueberries")
+            ),
+            CollapsableSection(
+                title = "Fruits C",
+                rows = listOf("Cherimoya", "Cantaloupe", "Cherries", "Clementine")
+            ),
+        ),
+    )
+    
+
 }
 
 @Composable
@@ -134,3 +168,61 @@ fun Buttons() {
         }
     }
 }
+
+@Composable
+fun CollapsableLazyColumn(
+    sections: List<CollapsableSection>,
+    modifier: Modifier = Modifier
+) {
+    val collapsedState = remember(sections) { sections.map { true }.toMutableStateList() }
+    LazyColumn(modifier) {
+        sections.forEachIndexed { i, dataItem ->
+            val collapsed = collapsedState[i]
+            item(key = "header_$i") {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .clickable {
+                            collapsedState[i] = !collapsed
+                        }
+                ) {
+                    Icon(
+                        Icons.Default.run {
+                            if (collapsed)
+                                KeyboardArrowDown
+                            else
+                                KeyboardArrowUp
+                        },
+                        contentDescription = "",
+                        tint = Color.LightGray,
+                    )
+                    Text(
+                        dataItem.title,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(vertical = 10.dp)
+                            .weight(1f)
+                    )
+                }
+                Divider()
+            }
+            if (!collapsed) {
+                items(dataItem.rows.size) { row ->
+                    Row {
+                        Spacer(modifier = Modifier.size(MaterialIconDimension.dp))
+                        Text(
+                            row.toString(),
+                            modifier = Modifier
+                                .padding(vertical = 10.dp)
+                        )
+                    }
+                    Divider()
+                }
+            }
+        }
+    }
+}
+
+data class CollapsableSection(val title: String, val rows: List<String>)
+
+const val MaterialIconDimension = 24f
