a5e67f6cda9920dac47647655810e11d2d08d6c4
Mati Mutz <mmutz@itba.edu.ar>
2023-11-22 21:09:34 -0300

    min change

--- app/src/main/java/ar/edu/itba/tpHciMobile/ui/screens/Routines.kt
+++ app/src/main/java/ar/edu/itba/tpHciMobile/ui/screens/Routines.kt
@@ -73,76 +73,20 @@ fun Routines(
     routinesViewModel: RoutinesViewModel,
     userViewModel: UserViewModel
 ) {
-    if (routinesViewModel.uiState.isFetchingRoutine) {
-        Loading()
+    if (LocalContext.current.resources.configuration.screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK >= Configuration.SCREENLAYOUT_SIZE_LARGE) {
+        RoutinesTablet(
+            modifier = modifier,
+            navController = navController,
+            routinesViewModel = routinesViewModel,
+            userViewModel = userViewModel
+        )
     } else {
-        if (routinesViewModel.uiState.routines == null)
-            routinesViewModel.getRoutinesOrderBy()
-
-        var routines = emptyList<Routine>()
-        if (!routinesViewModel.uiState.isFetchingRoutine)
-            routines = routinesViewModel.uiState.routines.orEmpty()
-
-        Surface(
-            color = Color(0xFFAEB0B2),
-            modifier = modifier.fillMaxHeight()
-        ) {
-            SwipeRefresh(
-                state = rememberSwipeRefreshState(isRefreshing = routinesViewModel.uiState.isFetchingRoutine),
-                onRefresh = { routinesViewModel.getRoutinesOrderBy() }
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.Start,
-                    verticalArrangement = Arrangement.Top
-                ) {
-                    Row() {
-                        Text(
-                            text = stringResource(R.string.routines),
-                            textAlign = TextAlign.Left,
-                            fontSize = 30.sp,
-                            fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
-                            modifier = Modifier.padding(8.dp)
-                        )
-                        Spacer(modifier = Modifier.weight(1f))
-                        OrderByBtn(
-                            routinesViewModel = routinesViewModel,
-                            modifier = modifier.padding(end = 8.dp)
-                        )
-                    }
-                    val list = routines
-                    if (list.isNotEmpty()) {
-                        LazyVerticalGrid(
-                            state = rememberLazyGridState(),
-                            columns = GridCells.Adaptive(minSize = 300.dp)
-                        ) {
-                            items(items = list) { routine ->
-                                Routine(
-                                    routine = routine,
-                                    routinesViewModel = routinesViewModel,
-                                    userViewModel = userViewModel,
-                                    onItemClick = {
-                                        if (!routinesViewModel.uiState.isFetchingRoutine) {
-                                            val isLiked =
-                                                routinesViewModel.uiState.routines?.find { it.id == routine.id }?.liked
-                                            routinesViewModel.getRoutine(
-                                                routine.id,
-                                                isLiked ?: false
-                                            )
-                                            navController.navigate(Screen.RoutineDetails.route + "/${routine.id}")
-                                        }
-                                    },
-                                    likeFunc = {
-                                        routinesViewModel.toggleLike(routine)
-                                    },
-                                )
-                            }
-                        }
-                    } else {
-                        Text(text = stringResource(R.string.no_routines))
-                    }
-                }
-            }
-        }
+        RoutinesPhone(
+            modifier = modifier,
+            navController = navController,
+            routinesViewModel = routinesViewModel,
+            userViewModel = userViewModel
+        )
     }
 }
 
@@ -155,24 +99,141 @@ fun Routine(
     likeFunc: () -> Unit,
     color: Color = Color.White
 ) {
-    if (LocalContext.current.resources.configuration.screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK >= Configuration.SCREENLAYOUT_SIZE_LARGE) {
-        RoutinesTablet(
-            routine = routine,
-            routinesViewModel = routinesViewModel,
-            userViewModel = userViewModel,
-            onItemClick = onItemClick,
-            likeFunc = likeFunc,
-            color = color
-        )
-    } else {
-        RoutinesPhone(
-            routine = routine,
-            routinesViewModel = routinesViewModel,
-            userViewModel = userViewModel,
-            onItemClick = onItemClick,
-            likeFunc = likeFunc,
-            color = color
-        )
+    var expanded = rememberSaveable { mutableStateOf(false) }
+    var fav = rememberSaveable { mutableStateOf(false) }
+    //luego para usar lo que hay en expanded se usa expanded.value
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        ), label = ""
+    )
+
+    val notLoggedIn = stringResource(R.string.not_logged_in)
+
+    Surface(
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .clickable(onClick = {
+                if (userViewModel.uiState.isAuthenticated) {
+                    onItemClick()
+                } else {
+                    Toast
+                        .makeText(
+                            MyApplication.instance,
+                            notLoggedIn,
+                            Toast.LENGTH_SHORT
+                        )
+                        .show()
+                }
+            }),
+        shape = MaterialTheme.shapes.medium,
+        color = color
+    ) {
+        Row(modifier = Modifier.padding(horizontal = 20.dp, vertical = 4.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding),
+            ) { //de esta manera se le da peso a la columna
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    if (color == Color.White) {
+                        Text(
+                            text = routine.name + " ",
+                            color = Color.Black,
+                            style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    } else {
+                        Text(
+                            text = routine.name + " ",
+                            color = Color.Black,
+                            style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold),
+                        )
+                    }
+                    Spacer(modifier = Modifier.weight(1f))
+                    Column() {
+                        if (userViewModel.uiState.isAuthenticated) {
+                            IconToggleButton(
+                                checked = routine.liked,
+                                onCheckedChange = { likeFunc(); },
+                            ) {
+                                Icon(
+                                    imageVector = if (routine.liked) Icons.Filled.Favorite else Icons.Filled.FavoriteBorder,
+                                    "Like",
+                                    modifier = Modifier.size(36.dp),
+                                    tint = Color.Black
+                                )
+                            }
+                        }
+                    }
+                }
+                Row() {
+                    ShowRatingBar(
+                        rating = routine.score,
+                        stars = 5,
+                        starsColor = Color.Yellow
+                    )
+                    Text(
+                        text = " (" + ((routine.score.toDouble() / 2)) + ")",
+                        color = Color.Black,
+                        style = MaterialTheme.typography.headlineMedium.copy(fontSize = 20.sp),
+                    )
+                }
+                Row {
+                    if (color == Color.White) {
+                        Text(
+                            text = routine.detail ?: "",
+                            color = Color.Black,
+                            style = MaterialTheme.typography.headlineMedium.copy(fontSize = 20.sp),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+
+                        )
+                    } else {
+                        Text(
+                            text = routine.detail ?: "",
+                            color = Color.Black,
+                            style = MaterialTheme.typography.headlineMedium.copy(fontSize = 20.sp),
+                        )
+                    }
+                }
+                Row {
+                    if (color == Color.White) {
+                        Text(
+                            text = stringResource(R.string.category) + ": " + routine.category.name,
+                            color = Color.Black,
+                            style = MaterialTheme.typography.headlineMedium.copy(fontSize = 20.sp),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    } else {
+                        Text(
+                            text = stringResource(R.string.category) + ": " + routine.category.name,
+                            color = Color.Black,
+                            style = MaterialTheme.typography.headlineMedium.copy(fontSize = 20.sp)
+                        )
+                    }
+                    Spacer(modifier = Modifier.weight(1f))
+                    Surface(
+                        shape = MaterialTheme.shapes.small,
+                        color = Color.LightGray
+                    ) {
+                        Text(
+                            text = ShowDifficulty(routine.difficulty ?: "").toString(),
+                            color = Color.Black,
+                            style = MaterialTheme.typography.headlineMedium.copy(fontSize = 20.sp),
+                            modifier = Modifier.padding(horizontal = 4.dp, vertical = 4.dp)
+                        )
+                    }
+                }
+            }
+        }
     }
 }
 
